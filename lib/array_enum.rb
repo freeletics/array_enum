867066319a4bafd78ec8399185f22695eb4b748b
Wojciech Wnętrzak <w.wnetrzak@gmail.com>
2019-01-10 14:15:34 +0100

    Add meaningful error message on missing value

--- lib/array_enum.rb
+++ lib/array_enum.rb
@@ -3,12 +3,17 @@ require "array_enum/subset_validator"
 require "array_enum/railtie" if defined?(Rails)
 
 module ArrayEnum
+  MISSING_VALUE_MESSAGE = "%{value} is not a valid value for %{attr}".freeze
+  private_constant :MISSING_VALUE_MESSAGE
+
   def array_enum(definitions)
     definitions.each do |attr_name, mapping|
       attr_symbol = attr_name.to_sym
 
       define_singleton_method("with_#{attr_name}".to_sym) do |values|
-        db_values = Array(values).map { |value| mapping.fetch(value.to_s) }
+        db_values = Array(values).map do |value|
+          mapping[value.to_s] || raise(ArgumentError, MISSING_VALUE_MESSAGE % {value: value, attr: attr_name})
+        end
         where("#{attr_name} @> ARRAY[:db_values]", db_values: db_values)
       end
 
@@ -17,7 +22,9 @@ module ArrayEnum
       end
 
       define_method("#{attr_name}=".to_sym) do |values|
-        self[attr_symbol] = Array(values).map { |value| mapping.fetch(value.to_s) }.uniq
+        self[attr_symbol] = Array(values).map do |value|
+          mapping[value.to_s] || raise(ArgumentError, MISSING_VALUE_MESSAGE % {value: value, attr: attr_name})
+        end.uniq
       end
     end
   end
